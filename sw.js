3c67e6530b690a40b86f94ad0b766f34ef8832fc
Amir Raza <amirraza6596695@gmail.com>
2026-03-28 21:49:36 -0700

    Install App

--- sw.js
+++ sw.js
@@ -4,12 +4,14 @@ const urlsToCache = [
   "/PORTFOLIO/index.html",
   "/PORTFOLIO/CSS/style.css",
   "/PORTFOLIO/JS/script.js",
-  "/PORTFOLIO/Assets/CodwWithAmir-favicone.png"
+  "/PORTFOLIO/Assets/CodeWithAmir-favicone.png" // Fix: spelling check karlo file ki
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      // force update ke liye
+      self.skipWaiting();
       return cache.addAll(urlsToCache);
     })
   );
@@ -18,6 +20,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
+      // Agar cache mein hai to wahan se lo, warna network se
       return response || fetch(event.request);
     })
   );
